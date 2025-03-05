8cced5c57dfae8bba5d6aa07907d6b37a45465e7
R230905 <afrizalfajri23@gmail.com>
2025-03-05 14:59:39 +0700

    welcome, user

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -105,49 +105,7 @@
                 </div>
             </form>
         </div>
-    </div>
-
-
-    {{-- Modal Edit User --}}
-    <div id="editModal" class="hidden fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-        <div class="bg-white p-6 rounded-lg w-1/3">
-            <h2 class="text-xl font-semibold">Edit User</h2>
-            
-            <form method="POST" id="editUserForm">
-                @csrf
-                <div class="grid gap-4 mb-4 grid-cols-2">
-                    <div class="col-span-2">
-                        <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
-                        <input type="text" name="name" id="userName" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Isikan nama pengguna" required="">
-                    </div>
-                    <div class="col-span-2 sm:col-span-1">
-                        <label for="price" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
-                        <input type="email" name="email" id="userEmail" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="jake@gmail.com" required="">
-                    </div>
-                    
-                    <div class="col-span-2 sm:col-span-1">
-                        <label for="category" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Role</label>
-                        <select id="userRole" name="role_id" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
-                            <option selected disabled>Pilih role</option>
-                            {{-- <option value="1">admin</option>
-                            <option value="2">dosen</option>
-                            <option value="3">alumni</option> --}}
-                            {{-- @foreach ($role as $r)
-                                <option value="{{ $r->id }}">
-                                    {{ $r->name }}
-                                </option>
-                            @endforeach --}}
-                        </select>
-                    </div>
-                </div>
-
-                <div class="flex justify-end mt-4">
-                    <button type="button" id="closeEditModal" class="px-4 py-2 bg-gray-400 text-white rounded">Batal</button>
-                    <button type="submit" class="px-4 py-2 bg-blue-500 text-white rounded ml-2">Simpan</button>
-                </div>
-            </form>
-        </div>
-    </div>
+    </div>   
     
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
@@ -156,29 +114,6 @@
             $(".create-user-btn").click(function() {
                 $("#createModal").removeClass("hidden");
             });
-
-            $(".edit-user-btn").click(function() {
-                let userId = $(this).data("id");
-                let userName = $(this).data("name");
-                let userEmail = $(this).data("email");
-                let userRole = $(this).data("role");
-
-                // Isi form modal dengan data dari tombol Edit
-                // $("#userIdInput").val(userId);
-                $("#userName").val(userName);
-                $("#userEmail").val(userEmail);
-                $("#userRole").val(userRole).change(); // Pilih role yang sesuai
-
-                // Set action form sesuai dengan route resource update
-                let updateUrl = "{{ route('admin.user.update', ':id') }}".replace(':id', userId);
-                $("#editUserForm").attr("action", updateUrl);
-
-                // Tampilkan modal
-                $("#editModal").removeClass("hidden");
-            });
-            $("#closeEditModal").click(function() {
-                $("#editModal").addClass("hidden");
-            })
             
             $("#closeCreateModal").click(function() {
                 $("#createModal").addClass("hidden");
