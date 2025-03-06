f077ddb1be67457e01a7ec406c6d7e563233171d
putro-24 <mars24112003@gmail.com>
2025-03-06 08:44:33 +0700

    landing

---
+++ resources/TopicListing-1.0.0/featured.php
@@ -0,0 +1,43 @@
+<?php
+// featured.php
+?>
+
+<section class="featured-section section-padding">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 col-12 text-center">
+                <h2 class="mb-4">Featured Topics</h2>
+            </div>
+
+            <div class="col-lg-4 col-md-6 col-12 mb-4">
+                <div class="featured-topic-block bg-white shadow-lg">
+                    <img src="images/topics/web_design.jpg" class="featured-topic-image img-fluid" alt="Web Design">
+                    <div class="featured-topic-content">
+                        <h5><a href="#">Web Design Basics</a></h5>
+                        <p>Learn the essentials of modern web design, including HTML, CSS, and UX principles.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6 col-12 mb-4">
+                <div class="featured-topic-block bg-white shadow-lg">
+                    <img src="images/topics/digital_marketing.jpg" class="featured-topic-image img-fluid" alt="Digital Marketing">
+                    <div class="featured-topic-content">
+                        <h5><a href="#">Digital Marketing</a></h5>
+                        <p>Explore SEO, social media marketing, and email campaigns to grow your audience.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6 col-12 mb-4">
+                <div class="featured-topic-block bg-white shadow-lg">
+                    <img src="images/topics/coding.jpg" class="featured-topic-image img-fluid" alt="Coding for Beginners">
+                    <div class="featured-topic-content">
+                        <h5><a href="#">Coding for Beginners</a></h5>
+                        <p>Start your coding journey with Python, JavaScript, and other beginner-friendly languages.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
