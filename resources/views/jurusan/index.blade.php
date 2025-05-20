0cf6fd38814911a3f46fd52b9eaedfe897b637c2
R230905 <afrizalfajri23@gmail.com>
2025-05-20 13:45:32 +0700

    jurusan

--- resources/views/jurusan/index.blade.php
+++ resources/views/jurusan/index.blade.php
@@ -9,13 +9,39 @@
                     <tr>
                         <th class="px-2 text-center py-3 border w-[40px]">NO</th>
                         <th class="px-2 text-center py-3 border">Nama Jurusan</th>
+                        <th class="px-2 text-center py-3 border">Aksi</th>
                     </tr>
                 </thead>
                 <tbody>
-                    @foreach ($jurusan as $index => $j)
+                    @foreach ($jurusan as  $j)
                     <tr class="bg-white border-b cursor-pointer jurusan-row" data-id="{{ $j->id }}">
-                        <td class="px-2 text-center py-4 border">{{ $index + 1 }}</td>
+                        <td class="px-2 text-center py-4 border">{{ $loop->iteration }}</td>
                         <td class="px-2 text-center py-4 border">{{ $j->nama_jurusan }}</td>
+                        <td class="px-2 text-center py-4 flex justify-center items-center">
+                            {{-- <a class="cursor-pointer edit-user-btn" data-id ="{{ $data->id }}">Edit</a> --}}
+                            <button data-id ="{{ $j->id }}" type="button" class="edit-user-btn text-yellow-400 border border-yellow-400 hover:bg-yellow-400 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-md text-sm p-2 text-center inline-flex items-center me-2">
+                                <svg class="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                                    <path fill-rule="evenodd" d="M11.32 6.176H5c-1.105 0-2 .949-2 2.118v10.588C3 20.052 3.895 21 5 21h11c1.105 0 2-.948 2-2.118v-7.75l-3.914 4.144A2.46 2.46 0 0 1 12.81 16l-2.681.568c-1.75.37-3.292-1.263-2.942-3.115l.536-2.839c.097-.512.335-.983.684-1.352l2.914-3.086Z" clip-rule="evenodd"/>
+                                    <path fill-rule="evenodd" d="M19.846 4.318a2.148 2.148 0 0 0-.437-.692 2.014 2.014 0 0 0-.654-.463 1.92 1.92 0 0 0-1.544 0 2.014 2.014 0 0 0-.654.463l-.546.578 2.852 3.02.546-.579a2.14 2.14 0 0 0 .437-.692 2.244 2.244 0 0 0 0-1.635ZM17.45 8.721 14.597 5.7 9.82 10.76a.54.54 0 0 0-.137.27l-.536 2.84c-.07.37.239.696.588.622l2.682-.567a.492.492 0 0 0 .255-.145l4.778-5.06Z" clip-rule="evenodd"/>
+                                </svg>                                                    
+                                <span class="sr-only">Icon description</span>
+                            </button>
+                            {{-- @if (!$data->roles->contains('name', 'admin')) --}}
+                                <form action="{{ route('admin.jurusan.destroy', $j->id) }}" method="POST">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit"
+                                        data-confirm-delete="true"
+                                        data-title="Hapus Jurusan?"
+                                        data-text="Yakin ingin menghapus data {{ $j->nama_jurusan }}?"
+                                        class="text-red-600 border border-red-600 hover:bg-red-600 hover:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-md text-sm p-2 text-center inline-flex items-center me-2">
+                                        <svg class="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+                                            <path fill-rule="evenodd" d="M8.586 2.586A2 2 0 0 1 10 2h4a2 2 0 0 1 2 2v2h3a1 1 0 1 1 0 2v12a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V8a1 1 0 0 1 0-2h3V4a2 2 0 0 1 .586-1.414ZM10 6h4V4h-4v2Zm1 4a1 1 0 1 0-2 0v8a1 1 0 1 0 2 0v-8Zm4 0a1 1 0 1 0-2 0v8a1 1 0 1 0 2 0v-8Z" clip-rule="evenodd"/>
+                                        </svg>          
+                                    </button>
+                                </form>
+                            {{-- @endif --}}
+                        </td>
                     </tr>
                     @endforeach
                 </tbody>
